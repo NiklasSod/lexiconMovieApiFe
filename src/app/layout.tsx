03d5cb28f656780e940c68f0dd2ddc2298985a77
NiklasSod <nma.prob@gmail.com>
2026-06-29 18:05:27 +0200

    init next project, updated versions, cleaned up files

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "MovieApiFe",
+  description: "A Next.js application",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.heading}>Welcome to MovieApiFe</h2>
+    </div>
+  );
+}
