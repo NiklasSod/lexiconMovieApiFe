21fadf650d41d602407a6c3bbb306d019af6c9fe
NiklasSod <nma.prob@gmail.com>
2026-08-16 17:30:25 +0200

    add prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,18 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: "MovieApiFe",
-  description: "A Next.js application",
-};
+  title: 'MovieApiFe',
+  description: 'A Next.js application',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
