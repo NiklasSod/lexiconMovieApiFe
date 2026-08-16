d8be10ddc8d2dd29576c116c71560d455485903a
NiklasSod <nma.prob@gmail.com>
2026-08-16 17:56:25 +0200

    add scss

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  sassOptions: {
+    additionalData: `@use "@/styles/variables" as *;`,
+  },
+}
+
+export default nextConfig
